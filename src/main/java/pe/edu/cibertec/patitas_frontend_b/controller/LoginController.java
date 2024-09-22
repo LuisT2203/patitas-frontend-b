b7b2dd82d09ac6c19652e5b68260adbc7b7a1db6
luis <luistran22@gmail.com>
2024-09-22 16:54:52 -0500

    hola

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_b/controller/LoginController.java
@@ -0,0 +1,66 @@
+package pe.edu.cibertec.patitas_frontend_b.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import pe.edu.cibertec.patitas_frontend_b.dto.LoginRequestDTO;
+import pe.edu.cibertec.patitas_frontend_b.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_frontend_b.viewmodel.LoginModel;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/inicio")
+    public String inicio(Model model){
+        LoginModel loginModel = new LoginModel("00","","");
+        model.addAttribute("loginModel", loginModel);
+        return "inicio";
+    }
+
+    @PostMapping("/autenticar")
+    public String autenticar(@RequestParam("tipoDocumento") String tipoDocumento,
+                             @RequestParam("numeroDocumento") String numeroDocumento,
+                             @RequestParam("password") String password,
+                             Model model) {
+        //validar campos de entrada
+        if(tipoDocumento == null || tipoDocumento.trim().length()==0
+                || numeroDocumento == null || numeroDocumento.trim().length()==0
+                || password == null || password.trim().length()==0){
+            LoginModel loginModel = new LoginModel("01","Error: Debe completar correctamente sus credenciales","");
+            model.addAttribute("loginModel", loginModel);
+            return "inicio";
+        }
+
+       try {
+           //Invocar Api
+           String endpoint = "http://localhost:8081/autenticacion/login";
+           LoginRequestDTO loginRequestDTO = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
+           LoginResponseDTO loginResponseDTO = restTemplate.postForObject(endpoint, loginRequestDTO, LoginResponseDTO.class );
+
+           //Validar respuesta
+           if(loginResponseDTO.codigo().equals("00")){
+               LoginModel loginModel = new LoginModel("00","",loginResponseDTO.nombreUsuario());
+               model.addAttribute("loginModel", loginModel);
+               return "principal";
+
+           }else{
+               LoginModel loginModel = new LoginModel("02","Error: Autenticacion fallida","");
+               model.addAttribute("loginModel", loginModel);
+               return "inicio";
+           }
+       } catch (Exception e) {
+           LoginModel loginModel = new LoginModel("99","Error: Ocurrio un problema en la autenticacion","");
+           model.addAttribute("loginModel", loginModel);
+           System.out.println(e.getMessage());
+           return "inicio";
+       }
+
+
+    }
+}
